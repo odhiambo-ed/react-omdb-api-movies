4cf4a9a2fb574bc99970e26f78662806022225be
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-09 00:37:57 +0300

    map movie data

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-function MovieList() {
+function MovieList( props ) {
   return (
-    <div>MovieList</div>
-  )
+    <div>
+      <h1>Movie List</h1>
+      <ul>
+        {props.movies.map((movie, index) => {
+          return (
+            <li key={index}>
+              <h2>{movie.Title}</h2>
+              <p>{movie.Year}</p>
+              <img src={movie.Poster} alt={movie.Title} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default MovieList
