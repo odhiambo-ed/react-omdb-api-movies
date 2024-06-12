822ac1f4207e312189f176df711c6a5cb98ec34a
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-12 22:16:55 +0300

    add watch list component

---
+++ src/components/AddWatchList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function AddWatchList() {
+  return (
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-bookmark"
+        viewBox="0 0 16 16"
+      >
+        <path d="M2 2v13.5l6-3.5 6 3.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2zM4 1h8a1 1 0 0 1 1 1v11.566l-5-2.916-5 2.916V2a1 1 0 0 1 1-1z" />
+      </svg>
+    </div>
+  );
+}
+
+export default AddWatchList;
