20e35aeca26de5f167608f5ce1d72ff7f9714f73
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-12 21:28:13 +0300

    update movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,28 +1,24 @@
-import React from 'react'
-import AddFavourite from './AddFavourite';
+import React from "react";
+import AddFavourite from "./AddFavourite";
 
-function MovieList( props ) {
+function MovieList(props) {
   return (
-    <div>
-      <h1>Movie List</h1>
-      <ul>
-        {props.movies.map((movie, index) => {
-          return (
-            <li key={index}>
-              <h6>{movie.Title}</h6>
-              <p>{movie.Year}</p>
-              <img src={movie.Poster} alt={movie.Title} />
-              <div
-                onClick={() => props.handleFavouritesClick(movie)}
-              >
-                <AddFavourite />
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+    <div className="movie-list">
+      {props.movies.map((movie, index) => (
+        <div key={index} className="movie-item">
+          <h6>{movie.Title}</h6>
+          <p>{movie.Year}</p>
+          <img src={movie.Poster} alt={movie.Title} />
+          <div
+            onClick={() => props.handleFavouritesClick(movie)}
+            className="favourite-icon"
+          >
+            <AddFavourite />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default MovieList
+export default MovieList;
