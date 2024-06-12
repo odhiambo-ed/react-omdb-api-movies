374e4175676cc888741e94cc9676ae66dadba5dd
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-12 22:15:50 +0300

    update app js

--- src/App.js
+++ src/App.js
@@ -4,30 +4,45 @@ import MovieList from './components/MovieList';
 import MovieHeader from './components/MovieHeader';
 import SearchBar from './components/SearchBar';
 import AddFavourite from './components/AddFavourite';
+import AddWatchList from './components/AddWatchList';
 
-function App(props) {
+function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   const [favourites, setFavourites] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+  const [type, setType] = useState('');
+  const [year, setYear] = useState('');
 
-  const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b160c966`;
+  const getMovieRequest = async (searchValue, type, year, page = 1) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&type=${type}&y=${year}&page=${page}&apikey=b160c966`;
 
-    const response = await fetch(url);
-    const responseJson = await response.json();
+    try {
+      const response = await fetch(url);
+      const responseJson = await response.json();
 
-    if (responseJson.Search) {
-      setMovies(responseJson.Search);
+      if (responseJson.Search) {
+        setMovies(responseJson.Search);
+      } else {
+        console.log('No results found');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
   };
 
   useEffect(() => {
-    getMovieRequest(searchValue);
-  }, [searchValue]);
+    getMovieRequest(searchValue, type, year);
+  }, [searchValue, type, year]);
 
   const addFavouriteMovie = (movie) => {
     const newFavouriteList = [...favourites, movie];
     setFavourites(newFavouriteList);
+  };
+
+  const addWatchlistMovie = (movie) => {
+    const newWatchlist = [...watchlist, movie];
+    setWatchlist(newWatchlist);
   };
 
   return (
@@ -35,12 +50,28 @@ function App(props) {
       <div className="row mb-4">
         <MovieHeader heading="Movies" />
         <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+        {/* Add filters for type and year */}
+        <div className="col-12">
+          <select onChange={(e) => setType(e.target.value)} className="form-control">
+            <option value="">All</option>
+            <option value="movie">Movies</option>
+            <option value="series">Series</option>
+            <option value="episode">Episodes</option>
+          </select>
+          <input
+            type="number"
+            placeholder="Year"
+            onChange={(e) => setYear(e.target.value)}
+            className="form-control"
+          />
+        </div>
       </div>
       <div className="row">
         <MovieList
           movies={movies}
           favouriteComponent={AddFavourite}
           handleFavouritesClick={addFavouriteMovie}
+          handleWatchlistClick={addWatchlistMovie}
         />
       </div>
       <div className="row mt-4">
@@ -49,6 +80,14 @@ function App(props) {
       <div className="row">
         <MovieList
           movies={favourites}
+        />
+      </div>
+      <div className="row mt-4">
+        <MovieHeader heading="Watchlist" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={watchlist}
         />
       </div>
     </div>
