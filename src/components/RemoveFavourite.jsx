de45213399258a12ac4a0e55b5c9542a6e573465
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-25 07:29:05 +0300

    edit removeFavourite

--- src/components/RemoveFavourite.jsx
+++ src/components/RemoveFavourite.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 function RemoveFavourite() {
   return (
@@ -8,11 +8,11 @@ function RemoveFavourite() {
         width="16"
         height="16"
         fill="currentColor"
-        class="bi bi-heart-fill"
+        className="bi bi-heart-fill"
         viewBox="0 0 16 16"
       >
         <path
-          fill-rule="evenodd"
+          fillRule="evenodd"
           d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
         />
       </svg>
@@ -20,4 +20,4 @@ function RemoveFavourite() {
   );
 }
 
-export default RemoveFavourite
+export default RemoveFavourite;
