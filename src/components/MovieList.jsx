4e896e4f32f104ea5abad6e42afb1e927fba14c4
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-25 07:28:33 +0300

    edit movieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,36 +1,40 @@
 import React from "react";
-import AddWatchList from "./AddWatchList";
 
-function MovieList(props) {
-  const FavouriteComponent = props.favouriteComponent;
+const MovieList = ({
+  movies,
+  favouriteComponent,
+  handleFavouritesClick,
+  handleWatchlistClick,
+  AddWatchlist,
+}) => {
+  const FavouriteComponent = favouriteComponent;
 
   return (
     <div className="movie-list">
-      {props.movies.map((movie, index) => (
-        <div key={index} className="movie-item">
-          <h6>{movie.Title}</h6>
-          <p>{movie.Year}</p>
-          <img src={movie.Poster} alt={movie.Title} />
-          <div className="d-flex flex-row justify-content-between">
-            <div
-              onClick={() => props.handleFavouritesClick(movie)}
-              className="favourite-icon"
-            >
-              <FavouriteComponent />
-            </div>
-            {props.handleWatchlistClick && (
-              <div
-                onClick={() => props.handleWatchlistClick(movie)}
-                className="watchlist-icon"
-              >
-                <AddWatchList />
-              </div>
-            )}
+      {movies.map((movie, index) => (
+        <div className="movie-item" key={movie.id}>
+          <h6>{movie.title}</h6>
+          <p>{movie.release_date}</p>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt="movie"
+          />
+          <div
+            onClick={() => handleFavouritesClick(movie)}
+            className="overlay d-flex align-items-center justify-content-center"
+          >
+            <FavouriteComponent />
+          </div>
+          <div
+            onClick={() => handleWatchlistClick(movie)}
+            className="overlay d-flex align-items-center justify-content-center"
+          >
+            <AddWatchlist />
           </div>
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default MovieList;
