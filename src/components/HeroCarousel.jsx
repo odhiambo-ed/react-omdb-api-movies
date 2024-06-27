e09e09653800394067d855ea3d5440f59981fed0
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-27 16:40:49 +0300

    add arrow to hero

--- src/components/HeroCarousel.jsx
+++ src/components/HeroCarousel.jsx
@@ -1,8 +1,30 @@
+// components/HeroCarousel.jsx
 import React from "react";
 import Slider from "react-slick";
 import { gsap } from "gsap";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./HeroCarousel.css";
 
 const HeroCarousel = ({ movies }) => {
+  const NextArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <div className="arrow next" onClick={onClick}>
+        &#10095;
+      </div>
+    );
+  };
+
+  const PrevArrow = (props) => {
+    const { onClick } = props;
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        &#10094;
+      </div>
+    );
+  };
+
   const settings = {
     dots: true,
     infinite: true,
@@ -13,8 +35,9 @@ const HeroCarousel = ({ movies }) => {
     autoplaySpeed: 3000,
     fade: true,
     cssEase: "linear",
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
     beforeChange: (current, next) => {
-      // Additional GSAP animations can be added here if needed
       const nextSlide = document.querySelector(`[data-index="${next}"]`);
       gsap.fromTo(nextSlide, { opacity: 0 }, { opacity: 1, duration: 1 });
     },
