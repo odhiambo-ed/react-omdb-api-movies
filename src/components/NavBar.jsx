adb9bbbf56dba9c09216a6e964161327d61f2cf8
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-27 18:57:40 +0300

    add navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { imdbLogo } from '../utils'
+import './NavBar.css'
 
 function NavBar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
-                <a className="navbar-brand" href="#">IMDB Clone</a>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4 logo">
+                {/* <a className="navbar-brand" href="#">IMDB Clone</a> */}
+                <img src={imdbLogo} alt="IMDB Logo" className="navbar-brand" />
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
